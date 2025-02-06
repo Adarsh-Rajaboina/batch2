8be8fe0ab1b96243d1ca19b8074c7a20fd7c43dc
Adarsh-Rajaboina <adarshrajaboina@gmail.com>
2025-02-06 12:31:41 +0530

    changes

---
+++ file.js
@@ -0,0 +1,47 @@
+let arr = [{
+    empid : 52486,
+    empname : "raju",
+    empcompany : "dell",
+    empsalary : 50000,
+    empaddress :{
+        empcity : "hyd",
+        emparea : "bahadurpally",
+    }
+
+},{
+    empid : 52487,
+    empname : "ram",
+    empcompany : "lenovo",
+    empsalary : 60000,
+    empaddress :{
+        empcity : "Andhra",
+        emparea : "araku",
+    }
+},{
+    empid : 52488,
+    empname : "akshay",
+    empcompany : "hp",
+    empsalary : 55000,
+    empaddress :{
+        empcity : "hyd",
+        emparea : "kukkatpally",
+    }
+},{
+    empid : 52489,
+    empname : "kiran",
+    empcompany : "apple",
+    empsalary : 65000,
+    empaddress :{
+        empcity : "tamilnadu",
+        emparea : "tiruvannamalai",
+    }
+},{
+    empid : 52490,
+    empname : "ramesh",
+    empcompany : "samsung",
+    empsalary : 57000,
+    empaddress :{
+        empcity : "andhra",
+        emparea : "tirupathi",
+    }
+}]
